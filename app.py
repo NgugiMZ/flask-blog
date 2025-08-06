96cf608b2db899df3f5ad768dacecd732a075739
NgugiMZ <freddngugi@gmail.com>
2025-08-06 16:55:19 +0300

    render fix

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def home():
     posts = models.Post.query.all()
     return render_template("home.html", posts=posts)
 
+#register route
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if current_user.is_authenticated:
@@ -37,6 +38,7 @@ def register():
         return redirect(url_for('login'))
     return render_template("register.html", form=form)
 
+#login route
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
@@ -53,11 +55,13 @@ def login():
             flash("Login unsuccessful. Check email and password.", "danger")
     return render_template("login.html", form=form)
 
+#logout route
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
+#post route
 @app.route("/create", methods=["GET", "POST"])
 @login_required
 def create_post():
@@ -69,6 +73,40 @@ def create_post():
         flash("Post created!", "success")
         return redirect(url_for('home'))
     return render_template("create_post.html", form=form)
+
+
+@app.route("/post/<int:post_id>/edit", methods=["GET", "POST"])
+@login_required
+def edit_post(post_id):
+    post = models.Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        flash("You can't edit this post.", "danger")
+        return redirect(url_for('home'))
+
+    form = PostForm(obj=post)
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        models.db.session.commit()
+        flash("Post updated!", "success")
+        return redirect(url_for('home'))
+
+    return render_template("edit_post.html", form=form, post=post)
+
+
+@app.route("/post/<int:post_id>/delete", methods=["POST"])
+@login_required
+def delete_post(post_id):
+    post = models.Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        flash("You can't delete this post.", "danger")
+        return redirect(url_for('home'))
+
+    models.db.session.delete(post)
+    models.db.session.commit()
+    flash("Post deleted!", "info")
+    return redirect(url_for('home'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
